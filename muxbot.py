581cb3499f326acb7b4847eb279c9df77a9d6059
mmagnetooooooo2 <95867923+mmagnetooooooo2@users.noreply.github.com>
2022-05-10 17:44:09 +0300

    Update muxbot.py

--- muxbot.py
+++ muxbot.py
@@ -1,4 +1,10 @@
 import logging
+import os
+import time
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from functions.utils import ReadableTime
+from pyromod import listen
 logging.basicConfig(level = logging.DEBUG,
                      format="%(asctime)s - %(name)s - %(message)s - %(levelname)s")
 
@@ -13,6 +19,12 @@ else:
 
 from helper_func.dbhelper import Database as Db
 db = Db().setup()
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+botStartTime = time.time()
 
 import pyrogram
 logging.getLogger('pyrogram').setLevel(logging.WARNING)
@@ -32,4 +44,31 @@ if __name__ == '__main__':
         api_hash = Config.API_HASH,
         plugins = plugins
     )
+async def start(self):
+        if not os.path.isdir(DOWNLOAD_LOCATION): os.makedirs(DOWNLOAD_LOCATION)
+        await super().start()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        LOGGER.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        if OWNER_ID != 0:
+            try:
+                await self.send_message(text="Karanlığın küllerinden yeniden doğdum.",
+                    chat_id=OWNER_ID)
+            except Exception as t:
+                LOGGER.error(str(t))
+
+    async def stop(self, *args):
+        if OWNER_ID != 0:
+            texto = f"Son nefesimi verdim.\nÖldüğümde yaşım: {ReadableTime(time.time() - botStartTime)}"
+            try:
+                if SEND_LOGS_WHEN_DYING:
+                    await self.send_document(document='log.txt', caption=texto, chat_id=OWNER_ID)
+                else:
+                    await self.send_message(text=texto, chat_id=OWNER_ID)
+            except Exception as t:
+                LOGGER.warning(str(t))
+        await super().stop()
+        LOGGER.info(msg="App Stopped.")
+        exit()
+
     app.run()
